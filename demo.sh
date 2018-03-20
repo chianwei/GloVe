59ad4bb79d5d3649e69e39a0a63b0ea015ba431d
Kevin Wei <kevinwei3@gmail.com>
2018-03-20 15:10:34 +0800

    change the corpus to my custom training file with wikipedia(20180101), twitter,createdebate and reasoning dataset

--- demo.sh
+++ demo.sh
@@ -15,7 +15,7 @@ if [ ! -e text8 ]; then
   rm text8.zip
 fi
 
-CORPUS=text8
+CORPUS=custom_train
 VOCAB_FILE=vocab.txt
 COOCCURRENCE_FILE=cooccurrence.bin
 COOCCURRENCE_SHUF_FILE=cooccurrence.shuf.bin
